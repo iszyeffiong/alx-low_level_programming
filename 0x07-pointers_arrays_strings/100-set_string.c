387abfc518cf27be3bc65e86fd02f097f4950929
iszyeffiong <israel.iszy@gmail.com>
2023-03-06 07:56:30 -0800

    that it

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
-* set_string - a function that sets the value of a pointer to a char.
-*@s: pointer value
-*@to: value pointed to
-*
-*/
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to change
+ * @to: string to change pointer to
+ *
+ * Return: void
+ */
 void set_string(char **s, char *to)
 {
 	*s = to;
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,27 +1,28 @@
-#include "main.h"
+#include <stdio.h>
+
 /**
-*print_diagsums -  a function that prints the sum of
-* the two diagonals of a square matrix of integers.
-*@a:matrix to be summed
-*@size:size of the matrix
+ * print_diagsums - print the sum of the two
+ *                 diagonals of a square matrix
+ *                 of integers.
+ * @a: input pointer
+ * @size: size of the matrix
+ *
+ * Return: nothing
 */
+
 void print_diagsums(int *a, int size)
 {
-	int i, sum1 = 0, sum2 = 0;
+	int i, S1, S2;
 
-	for (i = 0; i < size; i++)
+	S1 = 0;
+	S2 = 0;
+
+	for (i = 0; i < (size * size); i++)
 	{
-		sum1 += a[i];
-		a += size;
+		if (i % (size + 1) == 0)
+			S1 += a[i];
+		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
+			S2 += a[i];
 	}
-
-	a -= size;
-
-	for (i = 0; i < size; i++)
-	{
-		sum2 += a[i];
-		a -= size;
-	}
-
-	printf("%d, %d\n", sum1, sum2);
+	printf("%d, %d\n", S1, S2);
 }
