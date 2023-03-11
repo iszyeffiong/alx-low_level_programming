d4ade8dc0e40a59ad920e85b8c98332fc21b105c
iszyeffiong <israel.iszy@gmail.com>
2023-03-11 04:11:49 -0800

    Another day with jessica ALX

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to src, including \0
+ * @src: pointer
+ * @dest: pointer
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+
+	int i, length;
+	/**
+	 * length
+	 * dest
+	 */
+
+
+	for (length = 0; src[length] != '\0'; length++)
+
+	{
+
+	}
+
+	for (i = 0; i <= length; i++)
+
+	{
+
+		dest[i] = src[i];
+
+	}
+
+	return (dest);
+
+}
